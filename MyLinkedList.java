d316f90380b34b62c676b273ac50e09fda8124c7
kduong@stuy.edu <kduong@stuy.edu>
2018-12-13 10:48:48 -0500

    made one file

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,9 +1,36 @@
 public class MyLinkedList{
+
+  private class Node{
+   private int data;
+   private Node next,prev;
+
+   public Node (int a, Node nextref, Node prevref) {
+     data = a;
+     next = nextref;
+     prev = prevref;
+   }
+
+   public int getData() {
+     return data;
+   }
+
+   public Node next() {
+     return next;
+   }
+    Node prev()
+    void setNext(Node other)
+    void setPrev(Node other)
+    Integer getData()
+    Integer setData(Integer i)
+    String toString()
+
+  }
+
  private int size;
  private Node start,end;
 
  public int size() {
-   int count = 0;
+   get size;
 
  }
  public boolean add(int value);
