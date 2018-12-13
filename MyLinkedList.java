b6ae57c4430ff28e1c2d4c5a12a79e3ae14aa1ad
kduong@stuy.edu <kduong@stuy.edu>
2018-12-13 10:57:23 -0500

    finished Node class

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,38 +1,91 @@
 public class MyLinkedList{
 
   private class Node{
-   private int data;
-   private Node next,prev;
+    private Integer data;
+    private Node next,prev;
 
-   public Node (int a, Node nextref, Node prevref) {
-     data = a;
-     next = nextref;
-     prev = prevref;
-   }
+    public Node (int a, Node nextref, Node prevref) {
+      data = a;
+      next = nextref;
+      prev = prevref;
+    }
 
-   public int getData() {
-     return data;
-   }
+    public Node next() {
+      return next;
+    }
 
-   public Node next() {
-     return next;
-   }
-    Node prev()
-    void setNext(Node other)
-    void setPrev(Node other)
-    Integer getData()
-    Integer setData(Integer i)
-    String toString()
+    public Node prev() {
+      return prev;
+    }
+    public void setNext(Node other) {
+      next = other;
+    }
+
+    public void setPrev(Node other) {
+      prev = other;
+    }
+
+    public Integer getData() {
+      return data;
+    }
+
+    public Integer setData(Integer i) {
+      Integer old = data;
+      data = i;
+      return old;
+    }
+
+    public String toString() {
+      String s = "";
+      s += getData();
+      return s;
+    }
 
   }
 
- private int size;
- private Node start,end;
+  private int size;
+  private Node start,end;
 
- public int size() {
-   get size;
+  public boolean add(Integer value) {
+    Node n = new Node(value, end, null);
+    end = n;
+    return true;
+  }
 
- }
- public boolean add(int value);
- public String toString();
+  public int size() {
+    return size;
+  }
+
+  public String toString() {
+    
+  }   //note you don't have get(index) yet, nor would you want to use it here
+
+
+  public Integer get(int index){
+
+  }
+
+  public Integer set(int index,Integer value) {
+
+  }
+
+  public boolean contains(Integer value) {
+
+  }
+
+  public int indexOf(Integer value) {
+
+  }
+
+  public void add(int index,Integer value) {
+
+  }
+
+  public Integer remove(int index) {
+
+  }
+
+  public Integer remove(Integer value) {
+
+  }
 }
