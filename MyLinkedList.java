dee47a226e976eb43568393b326824e30cf17c1a
kduong@stuy.edu <kduong@stuy.edu>
2018-12-12 21:34:25 -0500

    confused

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -2,7 +2,10 @@ public class MyLinkedList{
  private int size;
  private Node start,end;
 
- public int size();
+ public int size() {
+   int count = 0;
+
+ }
  public boolean add(int value);
  public String toString();
 }
