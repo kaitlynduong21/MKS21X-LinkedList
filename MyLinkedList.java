e070f88005a9667000481ab2c84301eb8ea3e5e4
kduong@stuy.edu <kduong@stuy.edu>
2018-12-16 13:19:57 -0500

    fixed ssecond remove method

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -162,7 +162,7 @@ public class MyLinkedList{
   public Integer remove(int index) {
     Node current = start;
     int i = 0;
-    while (i < size - 1) {
+    while (i < size) {
       //System.out.println(this.toString());
       if (i == index) {
         Node n = current.next();
@@ -176,12 +176,23 @@ public class MyLinkedList{
       i ++;
       current = current.next();
     }
-    return -10;
+    return -10; //returns -10 if index is out of bounds
   }
 
-  /*public Integer remove(Integer value) {
-
-  }*/
+  public Integer remove(Integer value) {
+    Node current = start;
+      int i = 0;
+      while (i < size) {
+        if (current.getData().equals(value)) {
+          System.out.println("works");
+          this.remove(i);
+          return value;
+        }
+        i ++;
+        current = current.next();
+    }
+    return -10; //returns -10 if index is out of bounds
+  }
 
   public static void main(String[] args) {
       //What's poppin' everybody, welcome to my driver, we chill here in the code house
@@ -201,39 +212,16 @@ public class MyLinkedList{
       System.out.println("#######################");
       for(int i = 0; i < 10; i++) {
           list.add(i);
-          //System.out.println(list.size()); //<- Debugging
+          //System.out.println(list.size()); <- Debugging
       }
       System.out.println(list);
       System.out.println("This should print: \"[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]\"");
 
-      /*System.out.println(list.get(3));
-      System.out.println(list.get(5));
-      System.out.println(list.set(3, 5));
-      System.out.println(list.set(5, 3));
-      System.out.println(list);*/
-
-      //Step Six: Testing the Contains Method
-      System.out.println("##############################");
-      System.out.println("##Checking Containment Test:##");
-      System.out.println("##############################");
-      System.out.println(list.contains(999));
-      System.out.println("This should print: false");
-      System.out.println(list.contains(3));
-      System.out.println("This should print: true");
-
-      System.out.println("########################");
-      System.out.println("##Index of Value Test:##");
-      System.out.println("########################");
-      System.out.println(list.indexOf(0));
-      System.out.println("This should print: 0");
-      System.out.println(list.indexOf(5));
-      System.out.println("This should print: 5");
-
+      //Step Three: Adding a value to the middle
       System.out.println("#####################################");
       System.out.println("##Adding Values in the Middle Test:##");
       System.out.println("#####################################");
       list.add(5, 999);
-      list.add(15, 999);
       System.out.println(list);
       System.out.println("This should print: \"[0, 1, 2, 3, 4, 999, 5, 6, 7, 8, 9]\"");
 
@@ -244,5 +232,110 @@ public class MyLinkedList{
       list.remove(4);
       System.out.println(list);
       System.out.println("This should print: \"[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]\"");
-    }
+
+      //Step Five: Remove a value from the list by value
+      System.out.println("#####################################");
+      System.out.println("##Remove a Value by its Value Test:##");
+      System.out.println("#####################################");
+      System.out.println(list.remove(new Integer(9)));
+      System.out.println(list);
+      System.out.println("This should print: \"[0, 1, 2, 3, 4, 5, 6, 7, 8]\"");
+
+      //Step Six: Testing the Contains Method
+      System.out.println("##############################");
+      System.out.println("##Checking Containment Test:##");
+      System.out.println("##############################");
+      System.out.println(list.contains(new Integer(999)));
+      System.out.println("This should print: false");
+
+      //Step Seven: Testing the Size Method
+      System.out.println("##############");
+      System.out.println("##Size Test:##");
+      System.out.println("##############");
+      System.out.println(list.size());
+      System.out.println("This should print: 9");
+
+      //Step Eight: indexOf Method
+      System.out.println("########################");
+      System.out.println("##Index of Value Test:##");
+      System.out.println("########################");
+      System.out.println(list.indexOf(0));
+      System.out.println("This should print: 0");
+
+      //Step Nine: Exception Testing
+      System.out.println("######################");
+      System.out.println("##Exception Testing:##");
+      System.out.println("######################");
+      try {
+          list.remove(-1);
+      } catch(IndexOutOfBoundsException B) {
+          System.out.println("Removing a negative index throws the correct exception");
+      } catch(Exception e) {
+          System.out.println("Incorrect Exception Thrown:");
+          e.printStackTrace();
+      }
+      try {
+          list.remove(9000);
+      } catch(IndexOutOfBoundsException B) {
+          System.out.println("Removing an oversized index throws the correct exception");
+      } catch(Exception e) {
+          System.out.println("Incorrect Exception Thrown:");
+          e.printStackTrace();
+      }
+      try {
+          list.add(-1, new Integer(9));
+      } catch(IndexOutOfBoundsException B) {
+          System.out.println("Adding to a negative index throws the correct exception");
+      } catch(Exception e) {
+          System.out.println("Incorrect Exception Thrown:");
+          e.printStackTrace();
+      }
+      try {
+          list.add(90000, new Integer(69));
+      } catch(IndexOutOfBoundsException B) {
+          System.out.println("Adding to an oversized index throws the correct exception");
+      } catch(Exception e) {
+          System.out.println("Incorrect Exception Thrown:");
+          e.printStackTrace();
+      }
+      try {
+          list.set(-1, new Integer(9));
+      } catch(IndexOutOfBoundsException B) {
+          System.out.println("Setting a negative index throws the correct exception");
+      } catch(Exception e) {
+          System.out.println("Incorrect Exception Thrown:");
+          e.printStackTrace();
+      }
+      try {
+          list.set(90000, new Integer(69));
+      } catch(IndexOutOfBoundsException B) {
+          System.out.println("Setting an oversized index throws the correct exception");
+      } catch(Exception e) {
+          System.out.println("Incorrect Exception Thrown:");
+          e.printStackTrace();
+      }
+      try {
+          list.get(-1);
+      } catch(IndexOutOfBoundsException B) {
+          System.out.println("Getting a negative index throws the correct exception");
+      } catch(Exception e) {
+          System.out.println("Incorrect Exception Thrown:");
+          e.printStackTrace();
+      }
+      try {
+          list.get(90000);
+      } catch(IndexOutOfBoundsException B) {
+          System.out.println("Getting an oversized index throws the correct exception");
+      } catch(Exception e) {
+          System.out.println("Incorrect Exception Thrown:");
+          e.printStackTrace();
+      }
+
+//      Step 10: Edge Case Testing
+        System.out.println("######################");
+        System.out.println("##Edge-Case Testing:##");
+        System.out.println("######################");
+      System.out.println("Removing Last Element:");
+       list.remove(9);
+  }
 }
